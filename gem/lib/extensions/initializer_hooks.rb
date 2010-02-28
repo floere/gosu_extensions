5bc8bf6bf42c7ab43b6a71f556f206238b9d2b09
Florian Hanke <florian.hanke@gmail.com>
2010-02-28 20:15:17 +0100

    + initializer hooks

---
+++ gem/lib/extensions/initializer_hooks.rb
@@ -0,0 +1,26 @@
+# def initialize
+#   after_initialize
+# end
+# 
+module InitializerHooks
+  
+  mattr_accessor :hooks # { class => [blocks] }
+  self.hooks = {}
+  
+  # Calls the hooks in order of registration.
+  #
+  def after_initialize
+    hooks = InitializerHooks.hooks[self.class]
+    hooks && hooks.each do |hook|
+      self.instance_eval &hook
+    end
+  end
+  
+  # Registers a hook for a class.
+  #
+  def self.register klass, hook
+    self.hooks[klass] ||= []
+    self.hooks[klass] << hook
+  end
+  
+end
