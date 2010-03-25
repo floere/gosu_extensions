1c67762f0a28a6468aff316e8d2088b8f8f9e7c3
Florian Hanke <florian.hanke@gmail.com>
2010-03-25 23:44:59 +0100

    + executables, guso generator

---
+++ gem/generator/gogogosu.rb
@@ -0,0 +1,21 @@
+module Generator
+  class Gogogosu
+    
+    def initialize
+      @basedir = Dir.pwd
+    end
+    
+    # Generates a directory.
+    #
+    def directory name
+      
+    end
+    
+    # Generates a file. Will be rendered with erb.
+    #
+    def file name
+      
+    end
+    
+  end
+end
