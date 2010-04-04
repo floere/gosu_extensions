f2f7166f11999f2952ec735c5acbc193adfd4a11
Florian Hanke <florian.hanke@gmail.com>
2010-04-04 23:14:25 +0200

    + numeric spec

--- gem/spec/lib/extensions/numeric_spec.rb
+++ gem/spec/lib/extensions/numeric_spec.rb
@@ -4,7 +4,11 @@ describe Numeric do
   
   describe "radians_to_vec2" do
     it "should convert radians into a vector" do
-      10.0.radians_to_vec2.should == nil
+      10.0.radians_to_vec2.should be_kind_of(CP::Vec2)
+    end
+    it "should convert correctly" do
+      Math::PI.radians_to_vec2.x.should == -1.0
+      Math::PI.radians_to_vec2.y.should be_close 0.0, 0.000001
     end
   end
   
