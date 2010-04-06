2c8e8ae6c2f0a49f6cae24157218b665ff0304de
Florian Hanke <florian.hanke@gmail.com>
2010-04-06 11:59:07 +0200

    + shooter specs

--- gem/spec/lib/traits/shooter_spec.rb
+++ gem/spec/lib/traits/shooter_spec.rb
@@ -5,10 +5,22 @@ describe Shooter do
   before(:each) do
     @shooter = Class.new do
       include Shooter
-    end
+    end.new
   end
   it "should define a constant Shoot" do
     Shooter::Shoot.should == :shoot
+  end
+  it 'should have a reader shot_type' do
+    lambda { @shooter.shot_type }.should_not raise_error
+  end
+  it 'should have a writer shot_type' do
+    lambda { @shooter.shot_type = :some_shot_type }.should_not raise_error
+  end
+  it 'should have a writer shooting_range' do
+    lambda { @shooter.shooting_range = :some_range }.should_not raise_error
+  end
+  it 'should have a writer shooting_rate' do
+    lambda { @shooter.shooting_rate = :some_rate }.should_not raise_error
   end
   
 end
