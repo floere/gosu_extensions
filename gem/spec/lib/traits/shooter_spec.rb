f523b85adee43a264526b3a32192bae1ab03645b
Florian Hanke <florian.hanke@gmail.com>
2010-04-06 12:17:25 +0200

    + shooter specs

--- gem/spec/lib/traits/shooter_spec.rb
+++ gem/spec/lib/traits/shooter_spec.rb
@@ -3,9 +3,67 @@ require File.join(File.dirname(__FILE__), '/../../spec_helper')
 describe Shooter do
   
   before(:each) do
-    @shooter = Class.new do
-      include Shooter
-    end.new
+    @shooter = test_class_with(Shooter).new :some_window
+  end
+  
+  describe 'muzzle_rotation' do
+    context 'default' do
+      it 'should use the default calculation' do
+        @shooter.stub! :rotation => :some_rotation
+        
+        @shooter.muzzle_rotation.should == :some_rotation
+      end
+    end
+    context 'non-default' do
+      before(:each) do
+        @shooter_class = test_class_with(Shooter) do
+          muzzle_rotation { :some_rotation_calculation_result }
+        end
+      end
+      it 'should set the shooting_range' do
+        @shooter_class.new(:some_window).muzzle_rotation.should == :some_rotation_calculation_result
+      end
+    end
+  end
+  
+  describe 'muzzle_velocity' do
+    context 'default' do
+      it 'should use the default calculation' do
+        @shooter.stub! :rotation_vector => :some_velocity
+        
+        @shooter.muzzle_velocity.should == :some_velocity
+      end
+    end
+    context 'non-default' do
+      before(:each) do
+        @shooter_class = test_class_with(Shooter) do
+          muzzle_velocity { :some_velocity_calculation_result }
+        end
+      end
+      it 'should set the shooting_range' do
+        @shooter_class.new(:some_window).muzzle_velocity.should == :some_velocity_calculation_result
+      end
+    end
+  end
+  
+  describe 'muzzle_position' do
+    context 'default' do
+      it 'should use the default calculation' do
+        @shooter.stub! :position => "position + ", :rotation_vector => "rotation", :radius => 3
+        
+        @shooter.muzzle_position.should == "position + rotationrotationrotation"
+      end
+    end
+    context 'non-default' do
+      before(:each) do
+        @shooter_class = test_class_with(Shooter) do
+          muzzle_position { :some_position_calculation_result }
+        end
+      end
+      it 'should set the shooting_range' do
+        @shooter_class.new(:some_window).muzzle_position.should == :some_position_calculation_result
+      end
+    end
   end
   
   describe 'shoots' do
