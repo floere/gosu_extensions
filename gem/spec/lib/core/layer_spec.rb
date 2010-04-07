1bc75d44c0790d373d365171c3cbb2689bea1b96
Florian Hanke <florian.hanke@gmail.com>
2010-04-07 23:32:13 +0200

    + layer specs

---
+++ gem/spec/lib/core/layer_spec.rb
@@ -0,0 +1,16 @@
+require File.join(File.dirname(__FILE__), '/../../spec_helper')
+
+describe Layer do
+  it "should be the right layer" do
+    Layer::Background.should == 0
+  end
+  it "should be the right layer" do
+    Layer::Ambient.should == 1
+  end
+  it "should be the right layer" do
+    Layer::Players.should == 2
+  end
+  it "should be the right layer" do
+    Layer::UI.should == 3
+  end
+end
