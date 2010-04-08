edb0140fcee536591c3fddf86e308a5d5787e76b
Florian Hanke <florian.hanke@gmail.com>
2010-04-08 12:15:26 +0200

    + default wave position generation functions

--- gem/lib/core/wave.rb
+++ gem/lib/core/wave.rb
@@ -4,11 +4,23 @@ class Wave
   
   attr_reader :generated_type, :execution_amount, :positioning_function
   
-  UniformRandom = lambda { |window, instance| instance.warp_to *window.uniform_random_position }
-  TopBorder     = lambda { |window, instance| instance.warp_to rand(window.width), 0 }
-  RightBorder   = lambda { |window, instance| instance.warp_to window.width, rand(window.height) }
-  BottomBorder  = lambda { |window, instance| instance.warp_to rand(window.width), window.height }
-  LeftBorder    = lambda { |window, instance| instance.warp_to 0, rand(window.height) }
+  class << self
+    def uniform_random
+      lambda { |window, instance| instance.warp_to *window.uniform_random_position }
+    end
+    def top_border offset = 0
+      lambda { |window, instance| instance.warp_to rand(window.width), -offset }
+    end
+    def right_border offset = 0
+      lambda { |window, instance| instance.warp_to window.width+offset, rand(window.height) }
+    end
+    def bottom_border offset = 0
+      lambda { |window, instance| instance.warp_to rand(window.width), window.height+offset }
+    end
+    def left_border offset = 0
+      lambda { |window, instance| instance.warp_to -offset, rand(window.height) }
+    end
+  end
   
   #
   #
@@ -17,7 +29,7 @@ class Wave
   def initialize generated_type, execution_amount = 1, &positioning_function
     @generated_type      = generated_type
     @execution_amount    = execution_amount
-    @positioning_function = positioning_function || UniformRandom
+    @positioning_function = positioning_function || Wave.uniform_random
   end
   
   #
